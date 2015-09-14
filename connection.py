9d8e2bf1f9db0a16c77f3cf3a821b9b7413bd057
Ryan Norris <rynorris@gmail.com>
2015-09-14 17:54:52 +0100

    Add docstrings to connection.  Remove unused method.

--- connection.py
+++ connection.py
@@ -19,6 +19,7 @@ def hex_print(s, size=1):
 
 class Connection(object):
     """Represents a connection to a icecream scheduler."""
+
     CHUNK_SIZE = 2048
 
     def __init__(self, host, port=8765):
@@ -29,6 +30,11 @@ class Connection(object):
         self.connect()
 
     def connect(self):
+        """
+        Open a connection to the scheduler.
+
+        Also handles the initial procotol negociation. Currently badly.
+        """
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.server_host, self.server_port))
         log.debug("Sending protocol version 22")
@@ -41,6 +47,7 @@ class Connection(object):
         assert(self.socket.recv(4) == b'\x22\x00\x00\x00')
 
     def send(self, s):
+        """Reliably sends the string s to the scheduler."""
         log.debug("Sending:\n{0}".format(hex_print(s)))
         totalsent = 0
         while totalsent < len(s):
@@ -50,39 +57,27 @@ class Connection(object):
             totalsent += sent
 
     def send_message(self, msg):
+        """Packs and sends the given Message to the scheduler."""
         msg_string = msg.pack()
         self.send(struct.pack("!LL", len(msg_string), msg.msg_type) +
                   msg_string)
 
     def recv_chunk(self):
+        """Receives self.CHUNK_SIZE bytes from the wire and buffers them."""
         msg = self.socket.recv(self.CHUNK_SIZE)
         self.input_buf += msg
         return len(msg)
 
     def receive(self, n):
+        """Reliably receives n bytes from the wire."""
         while len(self.input_buf) < n:
             self.recv_chunk()
         s, self.input_buf = self.input_buf[:n], self.input_buf[n:]
         log.debug("Received:\n{0}".format(hex_print(s)))
         return s
 
-    def receive_until_null(self):
-        i = 0
-        while True:
-            if i < len(self.input_buf) and self.input_buf[i] == '\x00':
-                break
-
-            i += 1
-            if i >= len(self.input_buf):
-                amount_read = 0
-                while amount_read == 0:
-                    amount_read = self.recv_chunk()
-                    log.info("Read {0} bytes.".format(amount_read))
-
-        s, self.input_buf = self.input_buf[:i+1], self.input_buf[i+1:]
-        return s
-
     def get_message(self):
+        """Receive the next full Message from the wire."""
         length = struct.unpack("!L", self.receive(4))[0]
 
         log.debug("Receiving message of length {0}".format(length))
