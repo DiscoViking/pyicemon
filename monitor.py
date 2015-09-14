4dc908053b7161a360ce077fe2a77ce536dc7569
Ryan Norris <rynorris@gmail.com>
2015-09-14 18:02:01 +0100

    Add docstrings to Monitor

--- monitor.py
+++ monitor.py
@@ -13,6 +13,8 @@ log.addHandler(logging.NullHandler())
 
 
 class CS(object):
+    """Represents one active compilation node."""
+
     def __init__(self, id, name, ip, maxjobs):
         self.id = id
         self.name = name
@@ -25,6 +27,8 @@ class CS(object):
 
 
 class Job(object):
+    """Represents one active compile Job."""
+
     def __init__(self, id, filename, client_id, local=False):
         self.id = id
         self.filename = filename
@@ -39,6 +43,17 @@ class Job(object):
 
 
 class Monitor(object):
+    """
+    Monitor for an icecream cluster.
+
+    Owns a connection to the scheduler.
+    Maintains an in-memory view of the cluster state which is updated
+    in response to notification messages.
+
+    Also may have one or more publishers attached which are notified
+    whenever the state of the cluster changes.
+    """
+
     def __init__(self, host, port=8765):
         self.socket = None
         self.server_host = host
@@ -52,9 +67,11 @@ class Monitor(object):
         self.publishers = []
 
     def addPublisher(self, p):
+        """Add the publisher to the monitor."""
         self.publishers.append(p)
 
     def run(self):
+        """Main monitor loop.  Receives and handles messages one at a time."""
         while True:
             msg = self.conn.get_message()
 
@@ -79,6 +96,11 @@ class Monitor(object):
                 p.publish(self)
 
     def handleStats(self, msg):
+        """
+        Handle a Stats message.
+
+        Create/Destroy/Update the relevant CS as appropriate.
+        """
         if ("State" in msg.data and msg.data["State"] == "Offline"):
             # Destroy this CS if we have it.
             if msg.host_id in self.cs.keys():
@@ -99,11 +121,24 @@ class Monitor(object):
         self.cs[msg.host_id] = cs
 
     def handleGetCS(self, msg):
+        """
+        Handle a GetCS message.
+
+        This indicates the beginning of a new compile job, so create
+        a new Job to track it.
+        """
         job = Job(msg.job_id, msg.filename, msg.client_id)
         log.info("New job {0}".format(job))
         self.jobs[job.id] = job
 
     def handleJobBegin(self, msg):
+        """
+        Handle a JobBegin message.
+
+        We should have already created a Job when we got the GetCS message.
+        So look it up, and update it with the chosen CS.
+        Update the CS's active jobs.
+        """
         if msg.job_id not in self.jobs:
             log.warn("JobBegin received for unknown job {0}."
                      .format(msg.job_id))
@@ -117,6 +152,11 @@ class Monitor(object):
             self.cs[job.host_id].active_jobs += 1
 
     def handleJobDone(self, msg):
+        """
+        Handle a JobDone message.
+
+        Look up the Job, destroy it, and update the CS's active jobs.
+        """
         if msg.job_id not in self.jobs:
             log.warn("JobDone received for unknown job {0}."
                      .format(msg.job_id))
@@ -130,6 +170,14 @@ class Monitor(object):
             self.cs[job.host_id].active_jobs -= 1
 
     def handleLocalJobBegin(self, msg):
+        """
+        Handle a LocalJobBegin message.
+
+        This indicates the start of a new local job. (there won't have been
+        a GetCS since local jobs don't need to find a remote host.
+
+        So create the new job and update the CS's active jobs.
+        """
         job = Job(msg.job_id, msg.filename, msg.client_id, local=True)
         job.host_id = msg.client_id
         log.info("Created local job: {0}".format(job))
@@ -139,6 +187,11 @@ class Monitor(object):
             self.cs[job.client_id].active_jobs += 1
 
     def handleLocalJobDone(self, msg):
+        """
+        Handle a LocalJobDone message.
+
+        Look up the Job, destroy it, and update the CS's active jobs.
+        """
         if msg.job_id not in self.jobs:
             log.warn("LocalJobDone received for unknown job {0}."
                      .format(msg.job_id))
