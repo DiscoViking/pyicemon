d500a740f3c8c528c990b23c2b235b42b8aeb972
Ryan Norris <rynorris@gmail.com>
2015-09-17 13:45:28 +0100

    Add Monitor test to repro issue.

--- tests.py
+++ tests.py
@@ -1,6 +1,22 @@
 import unittest
 
 import messages
+from monitor import Monitor
+
+
+class DummyConnection(object):
+    next_msg = None
+    sent_msg = None
+
+    def __init__(self):
+        pass
+
+    def send_message(self, msg):
+        self.sent_msg = msg
+
+    def get_message(self):
+        return self.next_msg
+
 
 class TestPack(unittest.TestCase):
 
@@ -33,6 +49,33 @@ class TestPack(unittest.TestCase):
     def test_local_job_done(self):
         m = messages.LocalJobDoneMessage(2001)
         self.assertEqual(m.pack(), b'\x00\x00\x07\xd1')
+
+
+class TestMonitor(unittest.TestCase):
+
+    def test_negative_jobs(self):
+        """Test that a CS can't go into negative active jobs."""
+        m = Monitor(DummyConnection())
+
+        # Receive JobBeign messages before CS Stats.
+        m.handleGetCS(messages.GetCSMessage("file.c", 1, 1, 201))
+        m.handleJobBegin(messages.JobBeginMessage(1, 0, 101))
+
+        # Activate the CS.
+        stats = messages.StatsMessage(101, b'')
+        stats.data["Name"] = "cs1"
+        stats.data["IP"] = "1.1.1.1"
+        stats.data["MaxJobs"] = "4"
+        m.handleStats(stats)
+
+        # Receive a JobDone twice for the same job.
+        m.handleJobDone(messages.JobDoneMessage(1, 0,
+                                                0, 0, 0, 0,     # We don't care
+                                                0, 0, 0, 0, 0)) # about these.
+        m.handleJobDone(messages.JobDoneMessage(1, 0,
+                                                0, 0, 0, 0,     # We don't care
+                                                0, 0, 0, 0, 0)) # about these.
+        self.assertEqual(m.cs[101].active_jobs, 0)
 
 
 if __name__ == '__main__':
